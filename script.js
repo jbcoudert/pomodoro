cb489ae387ccf9c9fed00217a8f75532292c1ac5
tomsirat <sirat.tom@outlook.com>
2019-10-21 16:59:52 +0200

    Fin du premier jour

--- script.js
+++ script.js
@@ -1,25 +1,32 @@
-$(document).ready(function() {
+$(document).ready(function () {
     var timerInstance = new easytimer.Timer();
     var timer = timerInstance;
-    $('#start').click(function() {
-        timer.start({countdown: true, startValues: {seconds: 10}});
-        $('#countdownExample .values').html(timer.getTimeValues().toString());  
+    $('#start').click(function () {
+        timer.start({ countdown: true, startValues: { seconds: 10 } });
+        $('#countdownExample .values').html(timer.getTimeValues().toString());
         timer.addEventListener('secondsUpdated', function (e) {
             $('#countdownExample .values').html(timer.getTimeValues().toString());
         });
     })
-    $('#pause').click(function() {
-       timer.pause();
+
+    $('#pause').click(function () {
+        timer.pause();
     })
+
     timer.addEventListener('targetAchieved', function (e) {
         $('#countdownExample .values').html('KABOOM!!');
     });
 
-    $("#addTask").click(function() {
-        
+    $("#addTask").click(function () {
+
         var newTd = $("<td>")
-        $("#tr-2").append(newTd)
-        $(newTd).html($("#newTask").val())
+
+        $(".task3").append(newTd)
+        $(".task3").append($("#newTask").val() , $(".fa-trash"))
     })
+
+    $(".fa-trash").click(function () {
+        $(this).parent().remove();
+    });
 
 })
