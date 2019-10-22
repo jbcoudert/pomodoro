4be1bd1d05a0a088e18568167aee9285ffeb9c14
coudert <jean.baptiste.coudert1@gmail.com>
2019-10-22 10:37:22 +0200

    début 2eme journée

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 $(document).ready(function () {
     var timerInstance = new easytimer.Timer();
     var timer = timerInstance;
+    timer.start({ countdown: true, startValues: { seconds: 10 } });
+    $('#countdownExample .values').html(timer.getTimeValues().toString());
     $('#start').click(function () {
         timer.start({ countdown: true, startValues: { seconds: 10 } });
         $('#countdownExample .values').html(timer.getTimeValues().toString());
         timer.addEventListener('secondsUpdated', function (e) {
             $('#countdownExample .values').html(timer.getTimeValues().toString());
         });
+        
     })
 
     $('#pause').click(function () {
@@ -16,17 +19,34 @@ $(document).ready(function () {
     timer.addEventListener('targetAchieved', function (e) {
         $('#countdownExample .values').html('KABOOM!!');
     });
-
+    cpt = 0
     $("#addTask").click(function () {
+        cpt ++
+        var newLi = $("<li>", {
+            id : "li" + cpt
+        })
 
-        var newTd = $("<td>")
+        var newIcon = $("<i>", {
+            class : "fa fa-trash"
+        })
 
-        $(".task3").append(newTd)
-        $(".task3").append($("#newTask").val() , $(".fa-trash"))
+
+        $("#firstCol").append(newLi)
+        $(newLi).append($("#newTask").val())
+        $(newIcon).appendTo($(newLi))
+
+        $(newIcon).click(function () {
+            $(this).parent().remove();
+        });
     })
 
     $(".fa-trash").click(function () {
         $(this).parent().remove();
     });
 
+
+    $( function() {
+        $( "#firstCol" ).sortable();
+        $( "#firstCol" ).disableSelection();
+      } );
 })
